280c0f1c6a09030134f22ab82a229452c2e6aa5d
ohdowon064 <dhehdnjs03@gmail.com>
2022-03-22 11:24:00 +0900

    onclick 이벤트를 사용한 css 변경 - h1.style.color에 대해서 분기처리하여 클릭마다 폰트색변경

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    h1.style.color = "blue";
-    console.log("h1 was click!");
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue") {
+        newColor = "tomato";
+    }else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!"
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!"
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copy!")
-}
-
-function handleWindowOffline() {
-    alert("SOS no Wifi!")
-}
-
-function handleWindowOnline() {
-    alert("All good!")
-}
-
-h1.onclick = handleTitleClick
-h1.onmouseenter = handleMouseEnter
-h1.onmouseleave = handleMouseLeave
-
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.onclick = handleTitleClick;
