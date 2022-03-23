63c90c34a2f8de310c91c3b06526bef5525a383d
ohdowon064 <dhehdnjs03@gmail.com>
2022-03-23 12:24:00 +0900

    기존 class를 덮어씌워버리는 버그 발생

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    if(h1.className === "active") {
+    const clickedClass = "clicked";
+    if(h1.className === clickedClass) {
         h1.className = "";
     }else {
-        h1.className = "active";
+        h1.className = clickedClass;
     }
 }
 
