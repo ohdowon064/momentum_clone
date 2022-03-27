21e5624bdad0e0eb7b27fde52a24e2ef3a477616
ohdowon064 <dhehdnjs03@gmail.com>
2022-03-27 19:00:00 +0900

    css 파일 추가

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const loginInput = loginForm.querySelector("input");
 const link = document.querySelector("a")
 
 function onLoginSubmit(event) {
-    event.preventDefault(); // 기본동작(새로고침) 막음
+    event.preventDefault();
     console.dir(event);
 }
 
 function handleLinkClick(event) {
-    event.preventDefault(); // 기본동작(링크이동) 막음
+    event.preventDefault();
     console.log(event)
 }
 
