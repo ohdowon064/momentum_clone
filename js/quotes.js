d3da32ce3c654c75945e86cc9b2df42f56262704
ohdowon064 <dhehdnjs03@gmail.com>
2022-04-04 12:44:00 +0900

    명언 리스트 추가

---
+++ js/quotes.js
@@ -0,0 +1,39 @@
+// https://www.keepinspiring.me/famous-quotes/
+const quotes = [
+    {
+        "quote": "The journey of a thousand miles begins with one step.",
+        "author": "Lao Tzu"
+    },
+    {
+        "quote": "Tis better to have loved and lost than to have never loved at all.",
+        "author": "Alrded Lord Tennyson"
+    },
+    {
+        "quote": "That which does not kill us makes us stronger.",
+        "author": "Friedrich Nietzsche"
+    },
+    {
+        "quote": "Life is what happens when you’re busy making other plans.",
+        "author": "John Lennon"
+    },
+    {
+        "quote": "When the going gets tough, the tough get going.",
+        "author": "Joe Kennedy"
+    },
+    {
+        "quote": "You must be the change you wish to see in the world.",
+        "author": "Mahatma Gandhi"
+    },
+    {
+        "quote": "You only live once, but if you do it right, once is enough.",
+        "author": "Mae West"
+    },
+    {
+        "quote": "Tough times never last but tough people do.",
+        "author": "Robert H. Schuller"
+    },
+    {
+        "quote": "Get busy living or get busy dying.",
+        "author": "Stephen King"
+    },
+];
