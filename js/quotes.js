14ea0a175f4d9f28b5e0116394e310043af8f0e5
ohdowon064 <dhehdnjs03@gmail.com>
2022-04-06 13:44:00 +0900

    jslint 추가 후, 잘못된 object 형식 수정

--- js/quotes.js
+++ js/quotes.js
@@ -1,40 +1,40 @@
 // https://www.keepinspiring.me/famous-quotes/
 const quotes = [
     {
-        "quote": "The journey of a thousand miles begins with one step.",
-        "author": "Lao Tzu"
+        quote: "The journey of a thousand miles begins with one step.",
+        author: "Lao Tzu"
     },
     {
-        "quote": "Tis better to have loved and lost than to have never loved at all.",
-        "author": "Alrded Lord Tennyson"
+        quote: "Tis better to have loved and lost than to have never loved at all.",
+        author: "Alrded Lord Tennyson"
     },
     {
-        "quote": "That which does not kill us makes us stronger.",
-        "author": "Friedrich Nietzsche"
+        quote: "That which does not kill us makes us stronger.",
+        author: "Friedrich Nietzsche"
     },
     {
-        "quote": "Life is what happens when you’re busy making other plans.",
-        "author": "John Lennon"
+        quote: "Life is what happens when you’re busy making other plans.",
+        author: "John Lennon"
     },
     {
-        "quote": "When the going gets tough, the tough get going.",
-        "author": "Joe Kennedy"
+        quote: "When the going gets tough, the tough get going.",
+        author: "Joe Kennedy"
     },
     {
-        "quote": "You must be the change you wish to see in the world.",
-        "author": "Mahatma Gandhi"
+        quote: "You must be the change you wish to see in the world.",
+        author: "Mahatma Gandhi"
     },
     {
-        "quote": "You only live once, but if you do it right, once is enough.",
-        "author": "Mae West"
+        quote: "You only live once, but if you do it right, once is enough.",
+        author: "Mae West"
     },
     {
-        "quote": "Tough times never last but tough people do.",
-        "author": "Robert H. Schuller"
+        quote: "Tough times never last but tough people do.",
+        author: "Robert H. Schuller"
     },
     {
-        "quote": "Get busy living or get busy dying.",
-        "author": "Stephen King"
+        quote: "Get busy living or get busy dying.",
+        author: "Stephen King"
     },
 ];
 
