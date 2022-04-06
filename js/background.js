f0ce8c046ca6e9c07f5d6ec82845a682e77fa3b0
ohdowon064 <dhehdnjs03@gmail.com>
2022-04-07 01:52:44 +0900

    document img 태그 추가

--- js/background.js
+++ js/background.js
@@ -25,4 +25,7 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-console.log(chosenImage);
+const bgImage = document.createElement('img');
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
