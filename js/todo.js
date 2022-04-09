1ed1c8380b692062d0da53231dd2ed75333f53bd
ohdowon064 <dhehdnjs03@gmail.com>
2022-04-09 14:50:00 +0900

    입력 todo에 대해, todo list에 추가하는 기능 구현

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,22 @@
-const toDoForm = document.getElementById('todo-form');
-const toDoInput = toDoForm.querySelector('input');
-const toDoList = document.getElementById('todo-list');
+const todoForm = document.getElementById('todo-form');
+const todoInput = todoForm.querySelector('input');
+const todoList = document.getElementById('todo-list');
 
-function handleToDoSubmit(event) {
-  event.preventDefault();
-  const newTodo = toDoInput.value;
-  toDoInput.value = '';
-  console.log(newTodo, toDoInput.value);
+function paintTodo(newTodo) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+
+  li.appendChild(span);
+  span.innerText = newTodo;
+
+  todoList.appendChild(li);
 }
 
-toDoForm.addEventListener('submit', handleToDoSubmit);
+function handleTodoSubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = '';
+  paintTodo(newTodo);
+}
+
+todoForm.addEventListener('submit', handleTodoSubmit);
